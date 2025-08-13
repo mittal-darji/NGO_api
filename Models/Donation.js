3f888342c1fea473fa9382ba318f2b400f3d3121
mittal-darji <mittaldarji290@gmail.com>
2025-08-13 20:51:32 +0530

    create donation

---
+++ Models/Donation.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const donationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: false,
+  },
+  message: {
+    type: String,
+    required: false,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["PENDING", "SUCCESS", "FAILED"],
+    default: "SUCCESS",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Donation", donationSchema);
